1e4897fcd2e18547b3ed6424fd755761930299e3
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-05-04 02:58:48 +0300

    add AdaptiveLayout to dashview

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,20 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:nag/widgets/Desktop_Layout.dart';
+import 'package:nag/widgets/adaptive_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileCustomLayaout: (context) => const SizedBox(),
+        tabletCustomLayaout: (context) => const SizedBox(),
+        desktopCustomLayaout: (context) => const DesktopLayout(),
+      ),
+    );
   }
 }
