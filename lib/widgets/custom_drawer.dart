3444a6e69a01be869027d2994867fa0c04d68e46
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-05-05 20:39:26 +0300

    making CustomDrawer responsive using customscrollview

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nag/models/drawer_model.dart';
 import 'package:nag/utils/app_images.dart';
-import 'package:nag/widgets/drawer_item.dart';
 import 'package:nag/widgets/drawer_items_listview.dart';
 import 'package:nag/widgets/inactive_draweritem.dart';
 import 'package:nag/widgets/user_info_listTile.dart';
@@ -15,31 +15,41 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 28, right: 20),
-      child: Column(
-        children: [
-          const Card(
-            color: Color(0xffFAFAFA),
-            elevation: 0,
-            child: UserInfoListTile(
-              image: Assets.imageAvatar1,
-              title: "Lekan Okeowo",
-              subtitle: "demo@gmail.com",
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Card(
+              color: Color(0xffFAFAFA),
+              elevation: 0,
+              child: UserInfoListTile(
+                image: Assets.imageAvatar1,
+                title: "Lekan Okeowo",
+                subtitle: "demo@gmail.com",
+              ),
             ),
           ),
-          const SizedBox(
-            height: 8,
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
           ),
           const DrawerItemsListView(),
-          const Spacer(),
-          InactiveDrawerItem(
-              model: DrawerItemModel(
-                  title: "Setting system", image: Assets.imageSetting)),
-          InactiveDrawerItem(
-              model: DrawerItemModel(
-                  title: "Logout account", image: Assets.imageLogout)),
-          const SizedBox(
-            height: 48,
-          )
+          SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  InactiveDrawerItem(
+                      model: DrawerItemModel(
+                          title: "Setting system", image: Assets.imageSetting)),
+                  InactiveDrawerItem(
+                      model: DrawerItemModel(
+                          title: "Logout account", image: Assets.imageLogout)),
+                  const SizedBox(
+                    height: 48,
+                  )
+                ],
+              )),
         ],
       ),
     );
