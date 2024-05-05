ccc66d916464933cd30397d919ba7c03c9cf61c9
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-05-05 19:23:32 +0300

    refactor DrawerItemsListView

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nag/models/drawer_model.dart';
 import 'package:nag/utils/app_images.dart';
 import 'package:nag/widgets/drawer_item.dart';
+import 'package:nag/widgets/drawer_items_listview.dart';
 import 'package:nag/widgets/user_info_listTile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -9,21 +10,13 @@ class CustomDrawer extends StatelessWidget {
     super.key,
   });
 
-  static List<DrawerItemModel> drawerItems = [
-    DrawerItemModel(title: "Dashboard", image: Assets.imageDashboard),
-    DrawerItemModel(title: "My Transaction", image: Assets.imageMyTransaction),
-    DrawerItemModel(title: "Statistics", image: Assets.imageStatistics),
-    DrawerItemModel(title: "Wallet Account", image: Assets.imageWalletAccount),
-    DrawerItemModel(title: "My Investments", image: Assets.imageMyinvestments),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 28),
+    return const Padding(
+      padding: EdgeInsets.only(left: 20, right: 28),
       child: Column(
         children: [
-          const Card(
+          Card(
             color: Color(0xffFAFAFA),
             elevation: 0,
             child: UserInfoListTile(
@@ -32,19 +25,10 @@ class CustomDrawer extends StatelessWidget {
               subtitle: "demo@gmail.com",
             ),
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: drawerItems.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: DrawerItem(
-                  model: drawerItems[index],
-                ),
-              );
-            },
+          SizedBox(
+            height: 8,
           ),
+          DrawerItemsListView(),
         ],
       ),
     );
