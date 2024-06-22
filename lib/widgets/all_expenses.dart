e2f2aaadaef2374e9b12577fb7870c7208f164f9
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-22 16:40:39 +0300

    add row of AllExpencesitem

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nag/widgets/all_expences_header.dart';
+import 'package:nag/widgets/all_expences_item.dart';
 
 class AllExpences extends StatelessWidget {
   const AllExpences({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Column(
-        children: [
-          AllExpencesHeader(),
-          SizedBox(
-            height: 16,
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 40),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Column(
+          children: [
+            AllExpencesHeader(),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Expanded(child: AllExpencesitem()),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(child: AllExpencesitem()),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(child: AllExpencesitem()),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
