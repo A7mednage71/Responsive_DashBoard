1c8665dcb45ba24ac647f6aabd0d5a35d4e1bc99
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-24 02:44:57 +0300

    create QuickInvoiceBottomWidget with two buttoms

---
+++ lib/widgets/quick_invoice_bottom.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:nag/utils/app_styles.dart';
+
+class QuickInvoiceBottom extends StatelessWidget {
+  const QuickInvoiceBottom({
+    super.key,
+    required this.text,
+    this.textColor,
+    this.backgroundColor,
+  });
+  final String text;
+  final Color? textColor;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          shape: const WidgetStatePropertyAll(RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)))),
+          elevation: const WidgetStatePropertyAll(0),
+          backgroundColor: WidgetStatePropertyAll(
+              backgroundColor ?? const Color(0xff4EB7F2)),
+          padding:
+              const WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 20)),
+        ),
+        child: Text(
+          text,
+          style: AppStyles.semiBold18
+              .copyWith(color: textColor ?? const Color(0xffFFFFFF)),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/quick_invoice_widget.dart
+++ lib/widgets/quick_invoice_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nag/widgets/custom_backgroud_container.dart';
 import 'package:nag/widgets/custom_data_form.dart';
 import 'package:nag/widgets/latest_transaction.dart';
+import 'package:nag/widgets/quick_invoice_bottom.dart';
 import 'package:nag/widgets/quick_invoice_header.dart';
 
 class QuickInvoiceWidget extends StatelessWidget {
@@ -16,9 +17,29 @@ class QuickInvoiceWidget extends StatelessWidget {
           QuickInvoiceHeader(),
           LatestTransaction(),
           Divider(height: 48, thickness: 1, color: Color(0xffF1F1F1)),
-          CustomerDataForm()
+          CustomerDataForm(),
+          SizedBox(height: 24),
+          QuickInvoiceBottomWidget()
         ],
       ),
+    );
+  }
+}
+
+class QuickInvoiceBottomWidget extends StatelessWidget {
+  const QuickInvoiceBottomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        QuickInvoiceBottom(
+            text: "Add more details",
+            textColor: Color(0xff4EB7F2),
+            backgroundColor: Colors.white),
+        SizedBox(width: 24),
+        QuickInvoiceBottom(text: "Send Money")
+      ],
     );
   }
 }
