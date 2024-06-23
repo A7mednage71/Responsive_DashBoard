9ea03f8635a24fca24733cdb331664387e4f9fe0
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-23 04:30:01 +0300

    extract Custom Background Container

--- lib/widgets/all_expances_items_body.dart
+++ lib/widgets/all_expances_items_body.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:nag/models/all_expances_items_model.dart';
 import 'package:nag/utils/app_images.dart';
 import 'package:nag/widgets/all_expences_item.dart';
-import 'package:nag/widgets/is_active_expances_item.dart';
 
-class AllExpancesItemBody extends StatelessWidget {
+class AllExpancesItemBody extends StatefulWidget {
   const AllExpancesItemBody({
     super.key,
   });
@@ -30,9 +29,16 @@ class AllExpancesItemBody extends StatelessWidget {
   ];
 
   @override
+  State<AllExpancesItemBody> createState() => _AllExpancesItemBodyState();
+}
+
+class _AllExpancesItemBodyState extends State<AllExpancesItemBody> {
+  int iSselected = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Row(
-        children: items.asMap().entries.map(
+        children: AllExpancesItemBody.items.asMap().entries.map(
       (e) {
         int index = e.key;
         var value = e.value;
@@ -41,17 +47,35 @@ class AllExpancesItemBody extends StatelessWidget {
           return Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: AllExpancesItem(
-                iSactive: true,
-                expancesItemsModel: value,
+              child: GestureDetector(
+                onTap: () {
+                  if (iSselected != index) {
+                    setState(() {
+                      iSselected = index;
+                    });
+                  }
+                },
+                child: AllExpancesItem(
+                  iSactive: iSselected == index,
+                  expancesItemsModel: value,
+                ),
               ),
             ),
           );
         } else {
           return Expanded(
-              child: AllExpancesItem(
-            expancesItemsModel: value,
-            iSactive: false,
+              child: GestureDetector(
+            onTap: () {
+              if (iSselected != index) {
+                setState(() {
+                  iSselected = index;
+                });
+              }
+            },
+            child: AllExpancesItem(
+              expancesItemsModel: value,
+              iSactive: iSselected == index,
+            ),
           ));
         }
       },
--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:nag/widgets/all_expances_items_body.dart';
 import 'package:nag/widgets/all_expences_header.dart';
+import 'package:nag/widgets/custom_backgroud_container.dart';
 
 class AllExpences extends StatelessWidget {
   const AllExpences({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: const Column(
+    return const Padding(
+      padding: EdgeInsets.only(top: 40),
+      child: CustomBackgroundContainer(
+        child: Column(
           children: [
             AllExpencesHeader(),
             SizedBox(height: 16),
