643b8c64922da585d1789bc52315eaa66f9f0a25
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-25 14:17:08 +0300

    create DotIndicator

--- lib/widgets/Desktop_Layout.dart
+++ lib/widgets/Desktop_Layout.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nag/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:nag/widgets/custom_drawer.dart';
+import 'package:nag/widgets/dot_indicator.dart';
 import 'package:nag/widgets/my_card_page_view.dart';
 
 class DesktopLayout extends StatelessWidget {
