b25d0c94ae1adcaac14155cb532e81fa7e210e8f
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-26 15:16:16 +0300

    create and build income chart

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:nag/widgets/custom_backgroud_container.dart';
+import 'package:nag/widgets/income_chart.dart';
 import 'package:nag/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -11,6 +12,9 @@ class IncomeSection extends StatelessWidget {
         child: Column(
       children: [
         IncomeSectionHeader(),
+        Expanded(
+          child: IcomeChart(),
+        )
       ],
     ));
   }
