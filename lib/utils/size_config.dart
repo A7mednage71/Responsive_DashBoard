748573b9cb770915fb674cecd097f77f5eb5aa16
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-27 22:28:12 +0300

    add hight and width to size config

--- lib/utils/size_config.dart
+++ lib/utils/size_config.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/material.dart';
+
 class SizeConfig {
   static const double desktop = 1200;
   static const double tablet = 800;
+
+  static late double width, hight;
+
+  static init(BuildContext context) {
+    width = MediaQuery.sizeOf(context).width;
+    hight = MediaQuery.sizeOf(context).height;
+  }
 }
