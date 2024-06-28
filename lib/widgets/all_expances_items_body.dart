f3e3fc575b9bcc5c06cd196b2f74a655bbf63aa1
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-29 02:25:18 +0300

    fix AllExpancesItemBody widget

--- lib/widgets/all_expances_items_body.dart
+++ lib/widgets/all_expances_items_body.dart
@@ -8,6 +8,11 @@ class AllExpancesItemBody extends StatefulWidget {
     super.key,
   });
 
+  @override
+  State<AllExpancesItemBody> createState() => _AllExpancesItemBodyState();
+}
+
+class _AllExpancesItemBodyState extends State<AllExpancesItemBody> {
   static List<AllExpancesItemsModel> items = [
     AllExpancesItemsModel(
       image: Assets.imageBalance,
@@ -27,40 +32,85 @@ class AllExpancesItemBody extends StatefulWidget {
         date: "june 2024",
         price: r"$38,000")
   ];
-
-  @override
-  State<AllExpancesItemBody> createState() => _AllExpancesItemBodyState();
-}
-
-class _AllExpancesItemBodyState extends State<AllExpancesItemBody> {
   int iSselected = 0;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-        children: AllExpancesItemBody.items.asMap().entries.map(
-      (e) {
-        int index = e.key;
-        var value = e.value;
-        return Expanded(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: index == 1 ? 20 : 0),
-            child: GestureDetector(
-              onTap: () {
-                if (iSselected != index) {
-                  setState(() {
-                    iSselected = index;
-                  });
-                }
-              },
-              child: AllExpancesItem(
-                iSactive: iSselected == index,
-                expancesItemsModel: value,
-              ),
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              if (iSselected != 0) {
+                setState(() {
+                  iSselected = 0;
+                });
+              }
+            },
+            child: AllExpancesItem(
+              iSactive: iSselected == 0,
+              expancesItemsModel: items[0],
             ),
           ),
-        );
-      },
-    ).toList());
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              if (iSselected != 1) {
+                setState(() {
+                  iSselected = 1;
+                });
+              }
+            },
+            child: AllExpancesItem(
+              iSactive: iSselected == 1,
+              expancesItemsModel: items[1],
+            ),
+          ),
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              if (iSselected != 2) {
+                setState(() {
+                  iSselected = 2;
+                });
+              }
+            },
+            child: AllExpancesItem(
+              iSactive: iSselected == 2,
+              expancesItemsModel: items[2],
+            ),
+          ),
+        ),
+      ],
+    );
+    // return Row(
+    //     children: AllExpancesItemBody.items.asMap().entries.map(
+    //   (e) {
+    //     int index = e.key;
+    //     var value = e.value;
+    //     return Expanded(
+    //       child: Padding(
+    //         padding: EdgeInsets.symmetric(horizontal: index == 1 ? 20 : 0),
+    //         child: GestureDetector(
+    //           onTap: () {
+    //             if (iSselected != index) {
+    //               setState(() {
+    //                 iSselected = index;
+    //               });
+    //             }
+    //           },
+    //           child: AllExpancesItem(
+    //             iSactive: iSselected == index,
+    //             expancesItemsModel: value,
+    //           ),
+    //         ),
+    //       ),
+    //     );
+    //   },
+    // ).toList());
   }
 }
