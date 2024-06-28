0771abbf98eaa97d4927f5d5f74917400d415c2b
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-06-28 18:26:06 +0300

    run device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'views/dashboard_view.dart';
 
 void main() {
-  runApp(const ResponsiveDashboard());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const ResponsiveDashboard(),
+    ),
+  );
 }
 
 class ResponsiveDashboard extends StatelessWidget {
@@ -13,6 +20,8 @@ class ResponsiveDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(textTheme: GoogleFonts.montserratTextTheme()),
       home: const DashboardView(),
     );
